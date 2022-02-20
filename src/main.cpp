7e4ab30fa31a8b87c855fd5114c65b8de4aa102a
Bart Kessels <development@bk-mail.com>
2022-02-20 20:58:25 +0100

    Cleanup main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,25 @@
-#include <QApplication>
 #include <memory>
-#include <string>
+#include <QApplication>
 
 #include "domain/factories/RequestFactory.hpp"
 #include "presentation/windows/MainWindow.hpp"
-#include "service/implementations/CppRestRequestService.hpp"
 
-#include <iostream>
+using namespace getit;
 
 int main(int argc, char** args)
 {
     const auto& appName = "GetIt";
     QIcon icon(":icons/main");
 
-    const auto& requestFactory = std::make_shared<getit::domain::factories::RequestFactory>();
-    const auto& requestServiceFactory = std::make_shared<getit::service::factories::RequestServiceFactory>();
+    const auto& requestFactory = std::make_shared<domain::factories::RequestFactory>();
+    const auto& requestServiceFactory = std::make_shared<service::factories::RequestServiceFactory>();
 
     QApplication app(argc, args);
     QApplication::setWindowIcon(icon);
     QApplication::setApplicationDisplayName(appName);
     QApplication::setApplicationName(appName);
 
-    const auto& window = std::make_shared<getit::presentation::windows::MainWindow>(requestFactory, requestServiceFactory);
+    const auto& window = std::make_shared<presentation::windows::MainWindow>(requestFactory, requestServiceFactory);
     window->show();
 
     return QApplication::exec();
