12888bf490af55999da583d89900c21ce619a94c
Bart Kessels <development@bk-mail.com>
2022-03-21 11:56:21 +0100

    Continue working on MacOS packaging

--- packaging/macos/package.sh
+++ packaging/macos/package.sh
@@ -2,14 +2,27 @@
 
 # Compile GetIt
 echo 'Compiling GetIt...'
-cmake ../../.
-make GetIt
+cmake . -Dpackaging=true -G Ninja
+ninja GetIt
+
+# Libraries
+frameworksLocation="./bin/GetIt.app/Contents/Frameworks"
+mkdir -p "$frameworksLocation"
 
 # Link Qt libraries
 echo 'Linking Qt libraries'
+qtLibraryLocation="/usr/local/opt/qt/Frameworks"
+cp "$qtLibraryLocation/QtWidgets.framework" \
+   "$frameworksLocation"
+
+cp "$qtLibraryLocation/QtGui.framework" \
+   "$frameworksLocation"
+
 
 # Link CppRestSdk libraries
 echo 'Linking CppRestSdk libraries'
+cppRestLibraryLocation=""
 
-# Link Boost libraries
-echo 'Linking Boost libraries'
+#
+## Link Boost libraries
+#echo 'Linking Boost libraries'
