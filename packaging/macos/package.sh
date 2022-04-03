49c23fac87ec6aec2324af7cf685a03c642dd721
Bart Kessels <development@bk-mail.com>
2022-04-03 15:13:00 +0200

    Update MacOS packaging script

--- packaging/macos/package.sh
+++ packaging/macos/package.sh
@@ -2,27 +2,10 @@
 
 # Compile GetIt
 echo 'Compiling GetIt...'
-cmake . -Dpackaging=true -G Ninja
+# Disable warnings as error
+cmake . -Dpackaging=true -G Ninja -D CMAKE_CXX_FLAGS="-w"
 ninja GetIt
 
-# Libraries
-frameworksLocation="./bin/GetIt.app/Contents/Frameworks"
-mkdir -p "$frameworksLocation"
-
-# Link Qt libraries
-echo 'Linking Qt libraries'
-qtLibraryLocation="/usr/local/opt/qt/Frameworks"
-cp "$qtLibraryLocation/QtWidgets.framework" \
-   "$frameworksLocation"
-
-cp "$qtLibraryLocation/QtGui.framework" \
-   "$frameworksLocation"
-
-
-# Link CppRestSdk libraries
-echo 'Linking CppRestSdk libraries'
-cppRestLibraryLocation=""
-
-#
-## Link Boost libraries
-#echo 'Linking Boost libraries'
+# Link Qt
+echo 'Linking Qt to the app bundle'
+macdeployqt ./bin/GetIt.app -timestamp -always-overwrite -libpath=./_deps
