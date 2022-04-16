8eb7ee3a1d4a2449e22a0587af8916e5c03c5bbd
Bart Kessels <development@bk-mail.com>
2022-04-16 14:11:51 +0200

    Use powershell for packaging Windows executable

--- packaging/windows/package.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-cmake . -G Ninja
-ninja GetIt
-
-windeployqt ./bin/GetIt.exe
-7z a -tzip getit.zip ./bin/GetIt.exe
---
+++ packaging/windows/package.ps1
@@ -0,0 +1,5 @@
+Invoke-Expression cmake . -G Ninja
+Invoke-Expression ninja GetIt
+Invoke-Expression windeployqt ./bin/GetIt.exe
+
+Invoke-Expression 7z a -tzip getit.zip ./bin/GetIt.exe
