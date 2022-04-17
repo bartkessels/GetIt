a308d782b5d17c5c513ea28048d07c1f99b601f4
Bart Kessels <development@bk-mail.com>
2022-04-17 04:01:33 +0200

    Install flatpak runtimes

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -3,6 +3,10 @@
 # Compile GetIt
 cmake . -Dpackaging=true -G Ninja
 
+# Install required runtimes
+flatpak install org.kde.Platform
+flatpak install org.kde.Sdk
+
 # Build flatpak
 flatpak-builder --repo=getit_repo getit ./packaging/linux/getit.yml
 flatpak build-bundle getit_repo getit.flatpak net.bartkessels.getit
