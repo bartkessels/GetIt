867fb9b0f41d7a0d92800de7e0230062e444f595
Bart Kessels <development@bk-mail.com>
2022-04-17 03:51:28 +0200

    Remove compilation step when building GetIt flatpak

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -1,10 +1,5 @@
 #!/bin/env bash
 
-# Compile GetIt
-# Disable warnings as error
-cmake . -Dpackaging=true -G Ninja -D CMAKE_CXX_FLAGS="-w"
-ninja GetIt
-
 # Build flatpak
 flatpak-builder --repo=getit_repo getit ./packaging/linux/getit.yml
 flatpak build-bundle getit_repo getit.flatpak net.bartkessels.getit
