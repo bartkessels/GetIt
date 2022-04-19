d6501e782a0b0d0b78041ced6e5dda109008aa1c
Bart Kessels <development@bk-mail.com>
2022-04-19 07:54:43 +0200

    Add argument to CMake to locate Boost libraries

--- packaging/windows/package.ps1
+++ packaging/windows/package.ps1
@@ -1,5 +1,5 @@
-# Add extra argument to locate the vcpkg cmake location in the Github pipeline
-Invoke-Expression "cmake . -G Ninja -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake"
+# Add extra argument to locate the vcpkg cmake location in the Github pipeline and to include the boost libraries
+Invoke-Expression "cmake . -G Ninja -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake -DBoost_INCLUDE_DIR=C:\vcpkg\x64-windows-static\include"
 Invoke-Expression "ninja GetIt"
 Invoke-Expression "windeployqt ./bin/GetIt.exe"
 
